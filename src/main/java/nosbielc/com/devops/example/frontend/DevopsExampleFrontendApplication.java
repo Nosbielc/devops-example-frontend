7895c43e103cb3ddc7d4aaedbd2bfb4c5d66d696
Cleibson Gomes da Silva <c.gomes.da.silva@avanade.com>
2020-05-20 00:15:03 -0300

    updates pontuais

--- src/main/java/nosbielc/com/devops/example/frontend/DevopsExampleFrontendApplication.java
+++ src/main/java/nosbielc/com/devops/example/frontend/DevopsExampleFrontendApplication.java
@@ -2,6 +2,8 @@ package nosbielc.com.devops.example.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author Cleibson Gomes (https://github.com/Nosbielc) ON 19/05/2020
@@ -9,10 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class DevopsExampleFrontendApplication {
+public class DevopsExampleFrontendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsExampleFrontendApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DevopsExampleFrontendApplication.class);
     }
 
 }
